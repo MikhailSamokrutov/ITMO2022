9efd2138fbf300ba6ed08922610f7ae862201b37
MikhailSamokrutov <103500228+MikhailSamokrutov@users.noreply.github.com>
2022-05-18 17:20:36 +0300

    ITMO.StructType.ex3.2022
    ITMO.StructType.ex3.2022

---
+++ lab2/Zadanie3/Struct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.StructType.ex3._2022
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+
+    }
+
+
+    internal class Struct
+
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            
+            goldAccount.accType = AccountType.Checking;
+
+            goldAccount.accBal = (decimal)3200.00;
+
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter account number: ");
+
+            Console.WriteLine("*** Account Summary ***");
+
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+        }
+    }
+}
